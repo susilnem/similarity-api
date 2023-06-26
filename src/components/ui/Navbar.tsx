e5e014540308c45a719cd59d01d08c4a1b0b31e4
Susilnem <susilsaurav@gmail.com>
2023-06-26 21:44:58 +0545

    database schema and googleauth

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -4,9 +4,10 @@ import { buttonVariants } from "@/components/ui/Button";
 import SignInButton from "@/ui/SignInButton";
 import SignOutButton from "@/ui/SignOutButton";
 import ThemeToggle from "@/components/ThemeToggle";
+import { authOptions } from "@/lib/auth";
 
 const Navbar = async ({}) => {
-  const session = await getServerSession();
+  const session = await getServerSession(authOptions);
 
   return (
     <div className="fixed backdrop-blur-sm bg-white/75 dark:bg-slate-900/75 z-50 top-0 left-0 right-0 h-20 border-b border-slate-300 dark:border-slate-700 shadow-sm flex items-center justify-between">
